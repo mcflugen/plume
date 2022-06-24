561e51bc563ba76e5779453019167fd7306e4aec
mcflugen <mcflugen@gmail.com>
2022-06-23 21:12:22 -0600

    update to build extensions with cython_gsl

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
-from setuptools import setup, Extension
-
+from distutils.core import setup
+from Cython.Distutils import Extension
+from Cython.Distutils import build_ext
 import cython_gsl
+import pkg_resources
 
 
 setup(
+    include_dirs = [
+        cython_gsl.get_include(),
+        pkg_resources.resource_filename("numpy", "core/include"),
+    ],
+    cmdclass = {'build_ext': build_ext},
     ext_modules=[
         Extension(
             "plume.ext.centerline",
